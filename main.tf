09576257bd91bc30ce3fcccdb6b0bed3816c8d86
Emmanuel <emmanuel.okponobi@zedcrest.com>
2025-10-25 19:10:24 +0100

    Update redis default version

--- main.tf
+++ main.tf
@@ -76,7 +76,7 @@ resource "aws_elasticache_cluster" "redis-db" {
   engine               = "redis"
   node_type            = "cache.t3.micro" # Free tier eligible
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.2"
+  parameter_group_name = "default.redis6.x"
   engine_version       = "6.2"
   port                 = 6379
 
